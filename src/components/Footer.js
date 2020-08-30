98c0ab99bc9ea509d1d0b8f2e2f31c013aeb4740
ferueda <fe.rueda@outlook.com>
2020-08-29 20:31:55 -0400

    create Footer.js created the Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, Tooltip, Box, Text, Link } from '@chakra-ui/core';
+
+const Footer = () => {
+	return (
+		<Box w="100%" d="flex" justifyContent="center" justifyItems="center" mt="auto" mb="0.5rem">
+			<Text>
+				Made with ❤ by&nbsp;
+				<Link href="https://twitter.com/ferueda" isExternal>
+					Felipe Rueda
+				</Link>
+			</Text>
+		</Box>
+	);
+};
+
+export default Footer;
